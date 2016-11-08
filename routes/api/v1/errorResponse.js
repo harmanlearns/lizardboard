7391a5d6119cf7bad3d07c250f049a867dc7dc21
Harman Singh <harmanlearns@gmail.com>
2016-11-07 16:53:15 -0800

    Create User API(Closes #39) * Deleted routes/api/v1/widgets file, added creation of widgets to routes/api/v1/dashboards file * Converted widgets post to promises from callbacks * Created GET / POST / GET /:id PUT /:id and DELETE /:id routes for Users * Wrote error codes for different error types and required the errorResponse file in our dashboard routes file * Updated Users routes to handle errors with http error code responses

---
+++ routes/api/v1/errorResponse.js
@@ -0,0 +1,13 @@
+const errorResponse = response => error => {
+  if( error.name === 'CastError' ) {
+    response.status( 404 ).json({})
+  }
+  else if( error.name === 'ValidationError' ) {
+    response.status( 400 ).json({})
+  }
+  else {
+    response.status( 500 ).json({})
+  }
+}
+
+module.exports = errorResponse
