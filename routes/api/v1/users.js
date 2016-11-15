2ebad152d5b550301d97434173d5fae21d15ed3d
Monica Williams <monicaestellaw@gmail.com>
2016-11-14 17:28:07 -0800

    wrote GET, POST, PUT/:id, DELETE /:id, and GET /:id for users

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,14 +1,13 @@
 const User = require('../../../models/users.js')
-// const { DashboardWidget } = require('../../../models/dashboardwidgets.js')
 
 const express = require('express')
 const router = express.Router()
 
-// router.get('/', (request, response, next) => {
-//   User.find().exec()
-//     .then( dashboard => response.json( dashboard ) )
-//     .catch( error => next( error ) )
-// })
+router.get('/', (request, response, next) => {
+  User.find().exec()
+    .then( user => response.json( user ) )
+    .catch( error => next( error ) )
+})
 
 router.post('/', (request, response, next) => {
   User.create( request.body )
@@ -16,22 +15,22 @@ router.post('/', (request, response, next) => {
     .catch( error => next( error ) )
 })
 
-// router.get('/:id', (request, response, next) => {
-//   Dashboard.findById( request.params.id ).exec()
-//     .then( dashboard => response.json( dashboard ) )
-//     .catch( error => next( error ) )
-// })
-//
-// router.put('/:id', (request, response, next) => {
-//   Dashboard.findByIdAndUpdate( request.params.id, request.body ).exec()
-//     .then( dashboard => response.json( dashboard ) )
-//     .catch( error => next( error ) )
-// })
-//
-// router.delete('/:id',(request, response, next) => {
-//   Dashboard.findByIdAndRemove( request.params.id ).exec()
-//     .then( dashboard => response.json( dashboard ) )
-//     .catch( error => next( error ) )
-// })
+router.get('/:id', (request, response, next) => {
+  User.findById( request.params.id ).exec()
+    .then( user => response.json( user ) )
+    .catch( error => next( error ) )
+})
+
+router.put('/:id', (request, response, next) => {
+  User.findByIdAndUpdate( request.params.id, request.body ).exec()
+    .then( user => response.json( user ) )
+    .catch( error => next( error ) )
+})
+
+router.delete('/:id',(request, response, next) => {
+  User.findByIdAndRemove( request.params.id ).exec()
+    .then( user => response.json( user ) )
+    .catch( error => next( error ) )
+})
 
 module.exports = router
--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ app.use('/users', users);
 app.use( '/api/v1/dashboards', api.dashboards )
 app.use( '/api/v1/users', api.users )
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
